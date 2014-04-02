01450d5fec23e013e455614601032a22b5de2534
Manuel Omar Pineyro & Cody Wilson <student@epicodus.com>
2014-04-02 14:08:25 -0700

    next and previous now switch by number and not ID

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,24 +1,53 @@
 class Lesson < ActiveRecord::Base
   validates :name, :presence => true
   validates :number, :presence => true
+  belongs_to :section
+  default_scope order(:number)
 
   def next
-    @current_lesson = Lesson.find_by(number: number)
-    if number < Lesson.all.length
-      Lesson.find_by(number: number+1)
+    if number < Lesson.all.last.number
+      Lesson.all.detect {|lesson| lesson.number > number}
     else
-      @current_lesson
+      Lesson.find_by(number: number)
     end
   end
 
   def previous
-    @current_lesson = Lesson.find_by(number: number)
-    if number > 1
-      Lesson.find_by(number: number-1)
+    if number > Lesson.all.first.number
+      Lesson.all.detect {|lesson| lesson.number < number}
     else
-      @current_lesson
+      Lesson.find_by(number: number)
     end
   end
+
+# Working code that did not use detect or default scope --
+  # def next
+  #   ## detect ruby method
+  #   ## Find lessons by order based on number
+  #   @lessons_order = Lesson.order('number')
+  #   ## Get current Lesson object
+  #   @current_lesson = Lesson.find_by(number: number)
+  #   ## current lesson position in rturned ActivRecord object array
+  #   @current_lesson_position = @lessons_order.map(&:number).index(number)
+  #   if number < @lessons_order.last.number
+  #     ## look through ordered lessons returned from ActiveRecord, return one object
+  #     ## [limit] after the current object begins [offset]
+  #     @lessons_order.limit(1).offset(@current_lesson_position+1).first
+  #   else
+  #     @current_lesson
+  #   end
+  # end
+
+  # def previous
+  #   @lessons_order = Lesson.order('number')
+  #   @current_lesson = Lesson.find_by(number: number)
+  #   @current_lesson_position = @lessons_order.map(&:number).index(number)
+  #   if number == 1
+  #     @current_lesson
+  #   else
+  #     @lessons_order.limit(1).offset(@current_lesson_position-1).first
+  #   end
+  # end
 
 end
 
--- spec/less_spec.rb
+++ spec/less_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Lesson do
   it { should validate_presence_of :name}
   it { should validate_presence_of :number}
+  it { should belong_to :section}
 
   context '#next' do
     it 'returns the lesson with the next-highest number than the current lesson' do
@@ -13,13 +14,16 @@ describe Lesson do
     it 'returns the current lesson if the current lesson is the highest lesson' do
       current_lesson = Lesson.create({:name => 'lesson1', :number => 1})
       next_lesson = Lesson.create({:name => 'lesson2', :number => 2})
-      next_lesson.next.should eq next_lesson
+      last_lesson = Lesson.create({:name => 'lesson10', :number => 10})
+      # puts next_lesson.next.number
+      next_lesson.next.should eq last_lesson
     end
   end
     context '#previous' do
     it 'returns the lesson with the next-lowest number than the current lesson' do
       current_lesson = Lesson.create({:name => 'lesson1', :number => 1})
       next_lesson = Lesson.create({:name => 'lesson2', :number => 2})
+      # puts next_lesson.previous.number
       next_lesson.previous.should eq current_lesson
     end
       it 'returns the same lesson when the lesson is 1' do
